b111569eaf0a2a91477d4fe3c4fe83f0a1037295
saurabhjha69 <saurabhjhaforpc69@gmail.com>
2024-12-15 09:48:03 +0530

    Added Index Route

--- index.js
+++ index.js
@@ -15,38 +15,10 @@ app.use(express.static(path.join(dirname, "public")));
 app.use(express.json())
 app.use(cors())
 
-// Route to generate and send a QR code
-app.get("/", async (req, res) => {
-  const opts = {
-    errorCorrectionLevel: "H",
-    type: "image/png",
-    quality: 0.1,
-    margin: 1,
-    color: {
-      dark: "#010599FF",
-      light: "#FFBF60FF",
-    },
-  };
 
-  // Generate a unique file path for the QR code
-  
-
-  try {
-    // Generate QR code and save it to the file
-    await createQrCode(qrCodePath,"url")
-    console.log(`QR code saved to ${qrCodePath}`);
-    res.send(`<h1>QR Code Generated</h1><img src="/codes/${fileName}" alt="QR Code"/>`);
-  } catch (err) {
-    console.error("Failed to generate QR code:", err);
-    res.status(500).send("Failed to generate QR code.");
-  }
-});
-
-// Route to display a sample QR code (static example)
-app.get("/read", (req, res) => {
-  const qrCodeURL = "/codes/1734106628402.png"; // Point to a pre-existing image in "public/codes"
-  res.send(`<h1>View QR Code</h1><img src="${qrCodeURL}" alt="QR Code"/>`);
-});
+app.get('/',(req,res)=> {
+  res.send("Hi From Server!")
+})
 
 
 import { MenuRouter } from "./src/routes/MenuRouter.js";
